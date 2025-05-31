24a3724a77b395f25db462e98891ce9b9af692b6
pungu <thungxeng@gmail.com>
2025-05-31 13:29:40 +0800

    for vercel

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const port = 80;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get('/shop', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'shop.html'));
+app.get("/shop", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "shop.html"));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
